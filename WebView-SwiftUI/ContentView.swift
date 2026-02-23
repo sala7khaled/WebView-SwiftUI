f784e056a2d4c4e72afe344a1b6afe459b0d13b6
sala7khaled <sala7khalad@gmail.com>
2026-02-23 11:45:06 +0200

    Initial Commit

---
+++ WebView-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebView-SwiftUI
+//
+//  Created by Salah Khaled on 23/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebView-SwiftUI/WebView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_SwiftUIApp.swift
+//  WebView-SwiftUI
+//
+//  Created by Salah Khaled on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WebView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
